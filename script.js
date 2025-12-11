d54611f7a2ab3d6bd3c1f1920c681897ffe9ee7f
ilakkien2p <ilakkienselvakumar@gmail.com>
2025-12-11 20:52:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function f1(){
     name=document.getElementById("name").value;
-
-    
+    document.getElementById("display").innerHTML="happy birthday " + name;
+    document.getElementById("image").src="https://m.media-amazon.com/images/I/81hwYi+mnpL._AC_UF894,1000_QL80_.jpg";
 }
+
+document.getElementById("click").addEventListener("click", f1)
